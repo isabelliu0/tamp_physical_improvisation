5d4b725c2c8d5ddd09f2eec647a20c1e167bd892
Isabel Liu <yl3030@princeton.edu>
2024-10-29 08:03:57 -0400

    modified dependencies

--- tests/scripts/test_train_pushing_policy.py
+++ tests/scripts/test_train_pushing_policy.py
@@ -3,7 +3,7 @@
 from tamp_improv.scripts.train_pushing_policy import train_pushing_policy
 
 
-def test_train_pushing_policy(timesteps=1000):
+def test_train_pushing_policy(timesteps=1_000):
     """Test training pushing policy."""
     train_pushing_policy(
         total_timesteps=timesteps, seed=42, save_path="trained_policies/pushing_policy"
