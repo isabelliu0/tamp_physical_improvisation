1486299f632c35d39a5081d64ce13ddc4b851212
Isabel Liu <yl3030@princeton.edu>
2025-05-23 04:36:07 +0100

    redundant

--- bowen_test.sh
+++
@@ -1,4 +0,0 @@
-source /root/miniconda3/etc/profile.d/conda.sh
-conda activate improv
-
-pytest -s tests/approaches/test_graph_training.py -k test_multi_rl_cluttered_drawer_pipeline >> logs/test_graph_training.log
